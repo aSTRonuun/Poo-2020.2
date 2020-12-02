879b6be1c7a326a921fd20a1dac905e86f4d7848
aSTRonunn <vitormanoel00710@gmail.com>
2020-12-01 21:00:57 -0300

    initial project carr

--- Projeto 02 Java/carro/Carro.java
+++ Projeto 02 Java/carro/Carro.java
@@ -1,7 +1,59 @@
+import java.util.Scanner;
+
 public class Carro {
     
+    int gas;
+    int gasMax;
+    int pass;
+    int passMax;
+    int km;
+
+    public Carro(int gas, int pass){
+        this.gas = gas;
+        this.gasMax = 100;
+        this.pass = pass;
+        this.passMax = 2;
+        this.km = 0;
+    }
+
+    public boolean in(){
+        if(pass < passMax){
+            pass += 1;
+            return true;
+        }else{
+            System.out.println("fail: limite de pessoas atingido");
+            return false;
+        }
+    }
+
+    public String toString(){
+        return "pass: "
+            + pass
+            + ", gas: "
+            + gas
+            + ", km: "
+            + km;
+    }
+
+
+
     public static void main(String[] args) {
-        
-        System.out.println("Carro!");
+    
+        Carro car = new Carro(0, 0);
+        Scanner sc = new Scanner(System.in);
+    
+        while(true){
+            String line = sc.nextLine();
+            String[] usrIn = line.split(" ");
+            if(usrIn[0].equals("in")){
+                car.in();
+            }else if(usrIn[0].equals("show")){
+                System.out.println(car);
+            }else{
+                System.out.println("Comando invalido");
+            }
+        }
+
+
     }
 }

0b7c7d99aab15e0b917a7048216a2897b35717c5
aSTRonunn <vitormanoel00710@gmail.com>
2020-12-03 19:06:46 -0300

    add function drive and fix bugs

--- Projeto 02 Java/carro/Carro.java
+++ Projeto 02 Java/carro/Carro.java
@@ -39,10 +39,33 @@ public class Carro {
     void fuel(int value){
         if(gas < gasMax){
             gas += value;
-        }else if(gas > gasMax){
-            gas = gasMax;
+            if(gas > 100){
+                gas = gasMax;
+            }
         }
         
+    }
+
+    boolean drive(int distance){
+        if(pass > 0 && gas > 0){
+            
+            if(distance > gas){
+                km += gas;
+                System.out.println("fail: tanque vazio apos andar " + gas);
+                gas = 0;
+                return true;           
+            }else{
+                km += distance;
+                gas -= distance;
+                return true;  
+            }
+        }else if(pass == 0){
+            System.out.println("fail: nao ha ninguem no carro");
+            return false;
+        }else if(gas == 0){
+            System.out.println("fail: tanque vazio");
+        }
+        return true;
     }
 
     @Override
@@ -70,8 +93,13 @@ public class Carro {
             }else if(usrIn[0].equals("fuel")){
                 int qtd = Integer.parseInt(usrIn[1]);
                 car.fuel(qtd);
+            }else if(usrIn[0].equals("drive")){
+                int distance = Integer.parseInt(usrIn[1]);
+                car.drive(distance);
             }else if(usrIn[0].equals("show")){
                 System.out.println(car);
+            }else if(usrIn[0].equals("end")){
+                break;
             }else{
                 System.out.println("Comando invalido");
             }

930472afa8267053bfc740bd1b2c8d48ec4748da
aSTRonunn <vitormanoel00710@gmail.com>
2020-12-11 10:47:24 -0300

    finalizando exercicio riquinho

--- Notes/projetoRico_me/Riquinho01.java
+++ Notes/projetoRico_me/Riquinho01.java
@@ -1,9 +1,8 @@
 enum Moeda{
-
-    M10("0.10f", 1),
-    M25("0.25f", 2),
-    M50("0.50f", 3),
-    M100("1f", 4);
+    M10(0.10f, 1),
+    M25(0.25f, 2),
+    M50(0.50f, 3),
+    M100(1f, 4);
 
     float valor;
     int volume;
@@ -19,11 +18,11 @@ enum Moeda{
 
 }
 
-class item{
+class Item{
     String descricao;
     int volume;
 
-    itens(String descricao, int volume){
+    Item(String descricao, int volume){
         this.descricao = descricao;
         this.volume = volume;
     }
@@ -35,7 +34,7 @@ class item{
 }
 
 class Porco{
-    String itens = " ";
+    String itens = "";
     float valor = 0.0f;
     int volume = 0;
     int volumeMax;
@@ -45,7 +44,7 @@ class Porco{
         this.volumeMax = volumeMax;
     }
 
-    boolean adicionarDinheiro(Moeda Moeda){
+    boolean adicionarDinheiro(Moeda moeda){
         if(estaQuebrado){
             System.out.println("O porco esta quebrado");
             return false;
@@ -59,6 +58,56 @@ class Porco{
         return true;
     }
 
+    boolean adicionarItem(Item item){
+        if(estaQuebrado){
+            System.out.println("O porco esta quebrado");
+            return false;
+        }if(item.volume + this.volume > this.volumeMax){
+            System.out.println("Porco esta lotado");
+            return false;
+        }
+        this.volume += item.volume;
+        if(this.itens.equals(""))
+            this.itens = item.descricao;
+        else 
+            this.itens += ", " + item.descricao;
+        return true;
+    }
+
+
+    boolean quebrar(){
+        if(estaQuebrado){
+            System.out.println("O porco esta quebrado");
+            return false;
+        }
+        estaQuebrado = true;
+        return true;
+    }
+
+    float pegarDinheiro(){
+        if(!estaQuebrado){
+            System.out.println("Você deve quebrar o porco primeiro");
+            return 0.0f;
+        }
+        float aux = this.valor;
+        this.valor = 0;
+        return aux;
+    }
+
+    String pegarItem(){
+        if(!estaQuebrado){
+            System.out.println("Você deve quebrar o porco primeiro");
+            return "";
+        }
+        String aux = this.itens;
+        this.itens = "";
+        return aux;
+    }
+
+    public String toString(){
+        return itens + ":" + valor + ":" + volume + "/" + volumeMax + ":" + estaQuebrado;
+    }
+
 
 }
 
@@ -66,7 +115,13 @@ public class Riquinho01 {
     
     public static void main(String[] args) {
         Porco porco = new Porco(40);
-        Moeda moeda = Moeda.M100;
-        
+        System.out.println(porco);
+        porco.adicionarDinheiro(Moeda.M100);
+        porco.adicionarDinheiro(Moeda.M100);
+        System.out.println(porco);
+        porco.adicionarItem(new Item("cordao de ouro", 10));
+        System.out.println(porco);
+        porco.adicionarItem(new Item("pigente kunai", 5));
+        System.out.println(porco);
     }
 }

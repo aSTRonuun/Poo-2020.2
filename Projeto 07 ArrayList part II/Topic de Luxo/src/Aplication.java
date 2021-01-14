0e73beae9effd7794cf091f471f6b56d80817493
aSTRonunn <vitormanoel00710@gmail.com>
2021-01-13 21:07:32 -0300

    add interative main project07

--- Projeto 07 ArrayList part II/Topic de Luxo/src/Aplication.java
+++ Projeto 07 ArrayList part II/Topic de Luxo/src/Aplication.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
+
 import entities.Pass;
 import entities.Topic;
 
 public class Aplication {
 
     public static void main(String[] args) {
-        Topic topic = new Topic(3, 2);
+        Topic topic = new Topic(0, 0);
+        Scanner sc = new Scanner(System.in);
 
-        topic.toBoard(new Pass("davi", 17));
-        topic.toBoard(new Pass("gui", 80));
-        topic.toBoard(new Pass("ana", 35));
-        
-        System.out.println(topic);
+        while(true){
+            String line = sc.nextLine();
+            String[] usrIn = line.split(" ");
+            if(usrIn[0].equals("init")){
+                int seatsT = Integer.parseInt(usrIn[1]);
+                int seatsP = Integer.parseInt(usrIn[2]);
+                topic = new Topic(seatsT, seatsP);
+            }else if(usrIn[0].equals("in")){
+                String name = usrIn[1];
+                int age = Integer.parseInt(usrIn[2]);
+                Pass pass = new Pass(name, age);
+                topic.toBoard(pass);
+            }else if(usrIn[0].equals("out")){
+                String name = usrIn[1];
+                topic.land(name);
+            }else if(usrIn[1].equals("show")){
+                System.out.println(topic);
+            }else if(usrIn[1].equals("end")){
+                break;
+            }else{
+                System.out.println("Invalid Command...");
+            }
+        }
+        sc.close();
     }
 
 }
